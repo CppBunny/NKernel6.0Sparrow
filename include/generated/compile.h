c2ea437ab36fe0138886c7dc0959a699e2432d51
nnvt <aapbeer1@gmail.com>
2016-03-12 01:25:14 +0100

    Added Fiops Scheduler

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 19 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#16 SMP PREEMPT Thu Mar 10 22:07:25 CET 2016"
+#define UTS_VERSION "#19 SMP PREEMPT Fri Mar 11 22:22:26 CET 2016"
 #define LINUX_COMPILE_BY "noahvt"
 #define LINUX_COMPILE_HOST "NoahvtBuildServer"
 #define LINUX_COMPILER "gcc version 4.9.1 20140422 (prerelease) (Linaro GCC 2014.05) "
